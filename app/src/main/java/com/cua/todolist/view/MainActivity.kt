a29ca1515ae624b32c701a17d0240c1b8a68351f
CuaMcCarsaree44 <yosuakristianto754@gmail.com>
2019-05-03 16:55:29 +0700

    TabLayout and ViewPager
    Added TabLayout and ViewPager inside Card View

--- app/src/main/java/com/cua/todolist/view/MainActivity.kt
+++ app/src/main/java/com/cua/todolist/view/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.cua.todolist.view
 
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.cua.todolist.R
+import com.cua.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    fun init(){
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply{
+            mainTabLayout.addTab(mainTabLayout.newTab().setText("TODO List"))
+            mainTabLayout.addTab(mainTabLayout.newTab().setText("Reminder"))
+            mainTabLayout.addTab(mainTabLayout.newTab().setText("Exe"))
+
+        }
     }
 }
