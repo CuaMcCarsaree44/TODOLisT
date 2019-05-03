f0c6b828d26d0e4034f0cc7cd07f4c2b3021fa6c
CuaMcCarsaree44 <yosuakristianto754@gmail.com>
2019-05-03 15:58:14 +0700

    Moving Main Activity
    Moving Main Activity to view package

--- app/src/main/java/com/cua/todolist/view/MainActivity.kt
+++ app/src/main/java/com/cua/todolist/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.cua.todolist
+package com.cua.todolist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cua.todolist.R
 
 class MainActivity : AppCompatActivity() {
 
