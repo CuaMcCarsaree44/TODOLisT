d24acd3b5d2a46ef04c96c4ad08b83e49b2a0f33
CuaMcCarsaree44 <yosuakristianto754@gmail.com>
2019-05-06 11:36:25 +0700

    SplashScreenActivity Created
    Added SplashScreenActivity

---
+++ app/src/main/java/com/cua/todolist/view/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.cua.todolist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cua.todolist.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
