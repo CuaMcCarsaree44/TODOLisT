35080481ee56ce59d84bb1ea7ea8417d1d4f30eb
CuaMcCarsaree44 <yosuakristianto754@gmail.com>
2019-05-08 16:41:54 +0700

    Update Storage.kt
    Updated Storage for Dummy data. Will be delete after creating Room Database

--- app/src/main/java/com/cua/todolist/repository/Storage.kt
+++ app/src/main/java/com/cua/todolist/repository/Storage.kt
@@ -1,5 +1,6 @@
 package com.cua.todolist.repository
 
+import android.util.Log
 import com.cua.todolist.R
 import com.cua.todolist.model.Notes
 import kotlin.random.Random
@@ -8,21 +9,23 @@ class Storage {
     companion object {
         val x = arrayOf(R.color.color1, R.color.color2, R.color.color3,
             R.color.color4, R.color.color5, R.color.color6, R.color.color7,
-            R.color.color8, R.color.color9)
-        var r:Int = Random.nextInt(0, 8)
+            R.color.color8, R.color.color9, R.color.color0)
+        var r:Int = Random.nextInt(0, 9)
         val datas: ArrayList<Notes> = ArrayList()
         var note:Notes = Notes()
-        fun execute(){
-            for(i in 0..100){
-                r = Random.nextInt(0, 8)
-                note = Notes()
-                note.title = "Data Number $i"
-                note.priority = 0
-                note.content = "This is Data Number $i's description"
-                note.color = x[r]
-                datas.add(note)
-            }
+    }
+
+    init{
+        for(i in 0..100){
+            note = Notes()
+            note.title = "Data Number $i and ready to fly"
+            note.priority = 0
+            note.content = "This is Data Number $i's description"
+            note.color = x[i%10]
+
+            datas.add(note)
         }
+
     }
 
 
